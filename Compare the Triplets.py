c7c1d2716819fe00510fe332881303736022c91a
Julio Antonio <julioam766@gmail.com>
2024-02-08 23:46:12 +0700

    New commit

---
+++ Compare the Triplets.py
@@ -0,0 +1,26 @@
+def compareTriplets(a, b):
+    p1=0
+    p2=0
+    for x,y in zip(a, b):
+        if x>y :
+            p1+=1
+        elif y>x:
+            p2+=1
+        else:
+            p1+=0
+            p2+=0
+    return [p1, p2]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
