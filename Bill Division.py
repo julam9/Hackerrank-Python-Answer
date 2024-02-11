e4dc2380d05661ca6257a0bea3c3b5a8fee87cca
Julio Antonio <julioam766@gmail.com>
2024-02-12 00:16:48 +0700

    modified:   Bill Division.py

--- Bill Division.py
+++ Bill Division.py
@@ -1,7 +1,7 @@
 def bonAppetit(bill, k, b):
     total_anna = (sum(bill)-bill[k])//2
-    if total_anna > b:
-        return total_anna - b 
+    if total_anna < b:
+        return b - total_anna
     else:
         return "Bon Apetit"
     
