30c19172baf325d9ea79acfaa240160c4c304e21
Julio Antonio <julioam766@gmail.com>
2024-02-13 21:35:21 +0700

    modified :   Counting Valleys.py

--- Counting Valleys.py
+++ Counting Valleys.py
@@ -1,5 +1,16 @@
 def countingValleys(steps, path):
-    if path[steps] == "U":
-        n_valley = 1
+    p2 = list(path)
+    n_valley = 0
+    if p2[steps-1] == "U":
+       for i in range(steps):
+           if p2[i] == p2[i-1]:
+               n_valley =+ 1
+           else:
+               continue
     else:
         n_valley= 0
+
+    print(n_valley)
+
+countingValleys(8, "UDDDUDUU")
+
