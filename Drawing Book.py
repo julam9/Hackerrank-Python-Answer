9000df07ef42a11f57a024107e41b6930e6120de
Julio Antonio <julioam766@gmail.com>
2024-02-13 18:56:23 +0700

    modified:   Drawing Book.py

--- Drawing Book.py
+++ Drawing Book.py
@@ -1,9 +1,8 @@
 def pageCount(n, p):
     startfrom_1 = p//2  
-    if n%2 != 0:
-        startfrom_n = (n-p)//2 
+    startfom_n = n//2 - p//2
 
-
+    return min(startfrom_1, startfom_n)
 # testing
 pageCount(5,4)              
 print(2//2)
